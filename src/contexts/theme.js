64dbb22d778665151b57daf9902910f3855064e2
PramodSuthar <pramodsuthar@mail.com>
2023-12-21 22:42:45 -0400

    Added Support for multple dark mode themes

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -7,18 +7,49 @@ const ThemeProvider = ({ children }) => {
   const [themeName, setThemeName] = useState('light')
 
   useEffect(() => {
-    const isDark = localStorage.getItem('themeName') === 'dark'
+    const isDark =
+      localStorage.getItem('themeName') === 'dark' ? 'dark' : 'light'
     if (isDark) setThemeName('dark')
   }, [])
 
   const toggleTheme = () => {
-    const name = themeName === 'dark' ? 'light' : 'dark'
+    const name = themeName.includes('dark') ? 'light' : 'dark'
+    localStorage.setItem('themeName', name)
+    setThemeName(name)
+  }
+
+  const toggleRandomTheme = () => {
+    console.log('theme:', localStorage.getItem('themeName'))
+    const themeOptions = themeName.includes('dark')
+      ? ['dark', 'dark-1', 'dark-2', 'dark-3', 'dark-4']
+      : ['light', 'light-1', 'light-2', 'light-3', 'light-4']
+
+    // Retrieve the used options from local storage or initialize an empty array
+    const usedOptions = JSON.parse(localStorage.getItem('usedOptions')) || []
+
+    // If all options have been used, reshuffle the array
+    if (usedOptions.length === themeOptions.length) {
+      themeOptions.sort(() => Math.random() - 0.5)
+      usedOptions.length = 0 // Reset used options
+    }
+
+    // Find the first unused option
+    let name
+    do {
+      name = themeOptions.pop()
+    } while (usedOptions.includes(name))
+
+    // Update used options and the theme
+    usedOptions.push(name)
+    localStorage.setItem('usedOptions', JSON.stringify(usedOptions))
     localStorage.setItem('themeName', name)
     setThemeName(name)
   }
 
   return (
-    <ThemeContext.Provider value={[{ themeName, toggleTheme }]}>
+    <ThemeContext.Provider
+      value={[{ themeName, toggleTheme, toggleRandomTheme }]}
+    >
       {children}
     </ThemeContext.Provider>
   )
