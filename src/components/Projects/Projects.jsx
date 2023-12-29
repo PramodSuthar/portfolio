ba60d75c42d2f48280405f34dc2ddf4ef7ea47e9
PramodSuthar <67489670+PramodSuthar@users.noreply.github.com>
2023-12-29 04:29:28 -0400

    repackaged the files and updated package.json

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,32 +1,47 @@
+/* eslint-disable import/order */
 import { Fade } from 'react-awesome-reveal'
 import uniqid from 'uniqid'
+import Carousel from 'react-material-ui-carousel'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext'
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore'
 import { projects } from '../../portfolio'
 import ProjectContainer from '../ProjectContainer/ProjectContainer'
 import './Projects.css'
 
-const isDesktop = window.innerWidth > 768
+const isDesktop = window.innerWidth > 500
 
 const Projects = () => {
   if (!projects.length) return null
+
+  const projectsArray = () =>
+    isDesktop ? (
+      projects.map((project) => (
+        <Fade direction='up' triggerOnce='true'>
+          <ProjectContainer key={uniqid()} project={project} />
+        </Fade>
+      ))
+    ) : (
+      <span className='projects-grid box'>
+        <Carousel
+          fullHeightHover
+          navButtonsAlwaysVisible
+          PrevIcon={<NavigateBeforeIcon />}
+          NextIcon={<NavigateNextIcon />}
+          height='500px'
+        >
+          {projects.map((project) => (
+            <ProjectContainer key={uniqid()} project={project} />
+          ))}
+        </Carousel>
+      </span>
+    )
 
   return (
     <section id='projects' className='section projects'>
       <Fade direction='up' triggerOnce='true'>
         <h1 className='section-title'>Projects</h1>
       </Fade>
-      <div className='section-project sds'>
-        <span className='projects-grid box'>
-          {projects.map((project) => (
-            <Fade direction='up' triggerOnce='true'>
-              {isDesktop ? (
-                <></>
-              ) : (
-                <ProjectContainer key={uniqid()} project={project} />
-              )}
-            </Fade>
-          ))}
-        </span>
-      </div>
+      <div className='section-project sds'>{projectsArray()}</div>
     </section>
   )
 }
